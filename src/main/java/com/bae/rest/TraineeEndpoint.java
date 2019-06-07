17349027376d0d8b1cb1d72e53d800ea1a711720
JoeBenRob <joebenrob@gmail.com>
2019-06-07 14:27:04 +0100

    rest fill

--- src/main/java/com/bae/rest/TraineeEndpoint.java
+++ src/main/java/com/bae/rest/TraineeEndpoint.java
@@ -2,7 +2,10 @@ package com.bae.rest;
 
 import javax.inject.Inject;
 import javax.websocket.server.PathParam;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 
 import com.bae.business.TraineeService;
@@ -12,6 +15,12 @@ public class TraineeEndpoint {
 
 	@Inject
 	TraineeService service;
+
+	@Path("/createTrainee/{trainee}")
+	@POST
+	public String createTrainee(@PathParam("trainee") String trainee) {
+		return service.createTrainee(trainee);
+	}
 
 	@Path("/getTrainee/{id}")
 	@GET
@@ -24,4 +33,19 @@ public class TraineeEndpoint {
 	public String getAllTrainee() {
 		return service.getAllTrainee();
 	}
+
+	@Path("/updateTrainee/{id}")
+	@PUT
+	public String updateTrainee(@PathParam("id") int id) {
+		return null;
+
+	}
+
+	@Path("/deleteTrainee/{id}")
+	@DELETE
+	public String deleteTrainee(@PathParam("id") int id) {
+		return service.deleteTrainee(id);
+
+	}
+
 }
