d4eb3470ba5653af7bdf75b37bb1c781766e1be1
JoeBenRob <joebenrob@gmail.com>
2019-06-07 10:46:02 +0100

    persistence domain complete

---
+++ src/main/java/com/bae/persistence/domain/Classroom.java
@@ -0,0 +1,41 @@
+package com.bae.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Classroom {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int classroomId;
+	private String trainer;
+
+	public Classroom() {
+	}
+
+	public Classroom(int classroomId, String trainer) {
+		super();
+		this.classroomId = classroomId;
+		this.trainer = trainer;
+	}
+
+	public int getClassroomId() {
+		return classroomId;
+	}
+
+	public void setClassroomId(int classroomId) {
+		this.classroomId = classroomId;
+	}
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
+
+}
